a2def8e247dbc61d9ec2cf2566e22ea6c11a4376
soline-chanet <soline.chanet@gmail.com>
2014-05-12 16:51:53 -0400

    Initial_Commit

---
+++ Membranes/image_filename.m
@@ -0,0 +1,14 @@
+function data = image_filename(time_i, layer_i, src)
+% ** This is an automatically generated function
+% ** created at 5/10/2014, 2:27 PM by write_image_filename_function.m
+% ** Inputs the time, layer, and source directory of a data set.
+% ** Outputs the filename of that image.
+
+% ** For data set: Spn15-Rok-aPKC-Myo
+
+filename = 'CellsaPKCz4_z001.tif';
+
+z_name = sprintf(strcat('%.', num2str(3), 'u'), layer_i);
+filename(14:14+3-1) = z_name;
+
+data = fullfile(src, filename);
